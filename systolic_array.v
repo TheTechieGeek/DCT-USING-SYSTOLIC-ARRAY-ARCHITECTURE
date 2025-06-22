68b6fccf8de74399d414669f1e5ea69de09ac9f4
T G BALASUBRAMANIAM <157718312+TheTechieGeek@users.noreply.github.com>
2025-06-22 12:17:10 +0530

    systolic_array.v

--- systolic_array.v
+++ systolic_array.v
@@ -1,25 +1,30 @@
-`include "block.v"
-module systolic_array_1d_dct (
-    input clk,	// clock
-    input rst,	// reset value
-    input [15:0] in_north0,
-    input [15:0] in_west0,  // basis for PE0
-    input [15:0] in_west1,  // basis for PE1
-    input [15:0] in_west2,  // basis for PE2
-    input [15:0] in_west3,  // basis for PE3
 
-    output [31:0] result0,  // Y0
-    output [31:0] result1,  // Y1
-    output [31:0] result2,  // Y2
-    output [31:0] result3,  // Y3
+`include "block.v"
+
+module systolic_array_1d_dct #(
+    parameter DATA_WIDTH = 16    // 16 bit data width for the input 
+)(
+    input clk,  // clock
+    input rst,  // reset
+
+    input signed [DATA_WIDTH-1:0] in_north0,
+    input signed [DATA_WIDTH-1:0] in_west0,  // basis for PE0
+    input signed [DATA_WIDTH-1:0] in_west1,  // basis for PE1
+    input signed [DATA_WIDTH-1:0] in_west2,  // basis for PE2
+    input signed [DATA_WIDTH-1:0] in_west3,  // basis for PE3
+
+    output signed [(2*DATA_WIDTH)-1:0] result0,  // Y0
+    output signed [(2*DATA_WIDTH)-1:0] result1,  // Y1
+    output signed [(2*DATA_WIDTH)-1:0] result2,  // Y2
+    output signed [(2*DATA_WIDTH)-1:0] result3,  // Y3
     output reg done
 );
 
     // Internal wires to propagate data down
-    wire [15:0] out_south0, out_south1, out_south2;
+    wire signed [DATA_WIDTH-1:0] out_south0, out_south1, out_south2;
 
-    // PE0: input from top, first basis coefficient
-    block PE0 (
+    // PE0: First Processing element
+    block #(.DATA_WIDTH(DATA_WIDTH)) PE0 (
         .in_north(in_north0),
         .in_west(in_west0),
         .clk(clk), .rst(rst),
@@ -27,8 +32,8 @@ module systolic_array_1d_dct (
         .result(result0)
     );
 
-    // PE1: input from PE0, second basis coefficient
-    block PE1 (
+    // PE1: Input from PE0
+    block #(.DATA_WIDTH(DATA_WIDTH)) PE1 (
         .in_north(out_south0),
         .in_west(in_west1),
         .clk(clk), .rst(rst),
@@ -36,8 +41,8 @@ module systolic_array_1d_dct (
         .result(result1)
     );
 
-    // PE2: input from PE1
-    block PE2 (
+    // PE2: Input from PE1
+    block #(.DATA_WIDTH(DATA_WIDTH)) PE2 (
         .in_north(out_south1),
         .in_west(in_west2),
         .clk(clk), .rst(rst),
@@ -45,8 +50,8 @@ module systolic_array_1d_dct (
         .result(result2)
     );
 
-    // PE3: input from PE2
-    block PE3 (
+    // PE3: Input from PE2
+    block #(.DATA_WIDTH(DATA_WIDTH)) PE3 (
         .in_north(out_south2),
         .in_west(in_west3),
         .clk(clk), .rst(rst),
@@ -54,7 +59,7 @@ module systolic_array_1d_dct (
         .result(result3)
     );
 
-    //done logic after 4 cycles
+    // done logic after 4 cycles
     reg [2:0] count;
     always @(posedge clk or posedge rst) begin
         if (rst) begin
